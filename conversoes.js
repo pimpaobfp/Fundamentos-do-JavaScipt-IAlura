cba201b352268e655b6bac9f1d3784aa83ceff3e
Bruno <brunobothp@gmail.com>
2022-03-11 21:26:40 -0300

    Códigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado e como trabalhar junto dos boleanos 
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = '456';
+
+// Number();
+// String();
+console.log(numero + Number(numeroString));
+
+// conversão explicita
+
+Number();
+String();
---
+++ truthy-falsy.js
@@ -0,0 +1,27 @@
+// // boolean 
+// const usuarioLogado = true;
+// const contaPaga = false;
+
+// // truthy ou falsy
+
+// // 0 => falsy
+// // 1 => truthy
+
+// console.log(0 == false);
+// console.log ('' == false);
+// console.log(1 == true);
+
+//null => vazio ou nada e undefined
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = 'alura';
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+

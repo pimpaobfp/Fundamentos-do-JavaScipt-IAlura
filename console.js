c6b4a419a41d9af84b0159feb197dc31dc3cd067
Bruno <brunobothp@gmail.com>
2022-03-12 12:45:26 -0300

    erros e console

---
+++ console.js
@@ -0,0 +1,11 @@
+// // log => registro
+// console.log(123);
+// console.log('Eu sou Bruno');
+
+// const minhaVar = true;
+
+// console.log(minhaVar);
+
+// tratamento de erro
+
+console.error('Deu erro!');
---
+++ erros.js
@@ -0,0 +1,9 @@
+// const sem valor inicial
+const numero = 0;
+
+// naão fechar blocos
+console.log(numero);
+
+// var não definida
+const minhaVar = 'Oi!'
+console.log(minhaVar);
